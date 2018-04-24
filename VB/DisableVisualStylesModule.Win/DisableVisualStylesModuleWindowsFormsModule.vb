00a35870ca495602461811549b98c9adec4d9b64
DevExpressExampleBot <fakeEmail>
2018-04-24 22:19:56 +0300

    Source auto update [skip ci]

--- VB/DisableVisualStylesModule.Win/DisableVisualStylesModuleWindowsFormsModule.vb
+++ VB/DisableVisualStylesModule.Win/DisableVisualStylesModuleWindowsFormsModule.vb
@@ -1,13 +1,13 @@
-Imports Microsoft.VisualBasic
-Imports System
+﻿Imports System
 Imports DevExpress.ExpressApp
 Imports DevExpress.ExpressApp.Win.SystemModule
 
 Namespace DisableVisualStylesModule.Win
-	Public NotInheritable Class DisableVisualStylesModuleWindowsFormsModule
-		Inherits ModuleBase
-		Public Sub New()
-			RequiredModuleTypes.Add(GetType(SystemWindowsFormsModule))
-		End Sub
-	End Class
+    Public NotInheritable Class DisableVisualStylesModuleWindowsFormsModule
+        Inherits ModuleBase
+
+        Public Sub New()
+            RequiredModuleTypes.Add(GetType(SystemWindowsFormsModule))
+        End Sub
+    End Class
 End Namespace
--- VB/WinSolution.Win/WinApplication.vb
+++ VB/WinSolution.Win/WinApplication.vb
@@ -1,5 +1,4 @@
-Imports Microsoft.VisualBasic
-Imports System
+﻿Imports System
 Imports System.Collections.Generic
 Imports System.ComponentModel
 Imports DevExpress.ExpressApp.Win
@@ -8,26 +7,27 @@ Imports DevExpress.ExpressApp
 Imports DevExpress.ExpressApp.Xpo
 
 Namespace WinSolution.Win
-	Partial Public Class WinSolutionWindowsFormsApplication
-		Inherits WinApplication
-		Public Sub New()
-			InitializeComponent()
-		End Sub
-		Protected Overrides Sub CreateDefaultObjectSpaceProvider(ByVal args As CreateCustomObjectSpaceProviderEventArgs)
-			args.ObjectSpaceProvider = New XPObjectSpaceProvider(args.ConnectionString, args.Connection)
-		End Sub
-		Private Sub WinSolutionWindowsFormsApplication_DatabaseVersionMismatch(ByVal sender As Object, ByVal e As DevExpress.ExpressApp.DatabaseVersionMismatchEventArgs) Handles MyBase.DatabaseVersionMismatch
+    Partial Public Class WinSolutionWindowsFormsApplication
+        Inherits WinApplication
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+        Protected Overrides Sub CreateDefaultObjectSpaceProvider(ByVal args As CreateCustomObjectSpaceProviderEventArgs)
+            args.ObjectSpaceProvider = New XPObjectSpaceProvider(args.ConnectionString, args.Connection)
+        End Sub
+        Private Sub WinSolutionWindowsFormsApplication_DatabaseVersionMismatch(ByVal sender As Object, ByVal e As DevExpress.ExpressApp.DatabaseVersionMismatchEventArgs) Handles Me.DatabaseVersionMismatch
 #If EASYTEST Then
-			e.Updater.Update()
-			e.Handled = True
+            e.Updater.Update()
+            e.Handled = True
 #Else
-			If System.Diagnostics.Debugger.IsAttached Then
-				e.Updater.Update()
-				e.Handled = True
-			Else
-				Throw New InvalidOperationException("The application cannot connect to the specified database, because the latter doesn't exist or its version is older than that of the application." & Constants.vbCrLf & "This error occurred  because the automatic database update was disabled when the application was started without debugging." & Constants.vbCrLf & "To avoid this error, you should either start the application under Visual Studio in debug mode, or modify the " & "source code of the 'DatabaseVersionMismatch' event handler to enable automatic database update, " & "or manually create a database using the 'DBUpdater' tool." & Constants.vbCrLf & "Anyway, refer to the 'Update Application and Database Versions' help topic at http://www.devexpress.com/Help/?document=ExpressApp/CustomDocument2795.htm " & "for more detailed information. If this doesn't help, please contact our Support Team at http://www.devexpress.com/Support/Center/")
-			End If
+            If System.Diagnostics.Debugger.IsAttached Then
+                e.Updater.Update()
+                e.Handled = True
+            Else
+                Throw New InvalidOperationException("The application cannot connect to the specified database, because the latter doesn't exist or its version is older than that of the application." & ControlChars.CrLf & "This error occurred  because the automatic database update was disabled when the application was started without debugging." & ControlChars.CrLf & "To avoid this error, you should either start the application under Visual Studio in debug mode, or modify the " & "source code of the 'DatabaseVersionMismatch' event handler to enable automatic database update, " & "or manually create a database using the 'DBUpdater' tool." & ControlChars.CrLf & "Anyway, refer to the 'Update Application and Database Versions' help topic at http://www.devexpress.com/Help/?document=ExpressApp/CustomDocument2795.htm " & "for more detailed information. If this doesn't help, please contact our Support Team at http://www.devexpress.com/Support/Center/")
+            End If
 #End If
-		End Sub
-	End Class
+        End Sub
+    End Class
 End Namespace

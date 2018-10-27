800e990be037ae9778b46818a5f5005eaa8fbfa0
DevExpressExampleBot <fakeEmail>
2018-10-27 12:45:16 +0300

    Source auto update [skip ci]

---
+++ VB/DisableVisualStylesModule.Win/DisableVisualStylesModuleWindowsFormsModule.vb
@@ -0,0 +1,13 @@
+﻿Imports System
+Imports DevExpress.ExpressApp
+Imports DevExpress.ExpressApp.Win.SystemModule
+
+Namespace DisableVisualStylesModule.Win
+    Public NotInheritable Class DisableVisualStylesModuleWindowsFormsModule
+        Inherits ModuleBase
+
+        Public Sub New()
+            RequiredModuleTypes.Add(GetType(SystemWindowsFormsModule))
+        End Sub
+    End Class
+End Namespace
---
+++ VB/WinSolution.Win/WinApplication.Designer.vb
@@ -0,0 +1,46 @@
+﻿Namespace WinSolution.Win
+    Partial Public Class WinSolutionWindowsFormsApplication
+        ''' <summary> 
+        ''' Required designer variable.
+        ''' </summary>
+        Private components As System.ComponentModel.IContainer = Nothing
+
+        ''' <summary> 
+        ''' Clean up any resources being used.
+        ''' </summary>
+        ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso (components IsNot Nothing) Then
+                components.Dispose()
+            End If
+            MyBase.Dispose(disposing)
+        End Sub
+
+        #Region "Component Designer generated code"
+
+        ''' <summary> 
+        ''' Required method for Designer support - do not modify 
+        ''' the contents of this method with the code editor.
+        ''' </summary>
+        Private Sub InitializeComponent()
+            Me.module1 = New DevExpress.ExpressApp.SystemModule.SystemModule()
+            Me.module2 = New DevExpress.ExpressApp.Win.SystemModule.SystemWindowsFormsModule()
+            Me.visualStylesModuleWindowsFormsModule1 = New DisableVisualStylesModule.Win.DisableVisualStylesModuleWindowsFormsModule()
+            CType(Me, System.ComponentModel.ISupportInitialize).BeginInit()
+            ' 
+            ' WinSolutionWindowsFormsApplication
+            ' 
+            Me.ApplicationName = "WinSolution"
+            Me.Modules.Add(Me.module1)
+            Me.Modules.Add(Me.module2)
+            Me.Modules.Add(Me.visualStylesModuleWindowsFormsModule1)
+            CType(Me, System.ComponentModel.ISupportInitialize).EndInit()
+
+        End Sub
+        #End Region
+
+        Private module1 As DevExpress.ExpressApp.SystemModule.SystemModule
+        Private module2 As DevExpress.ExpressApp.Win.SystemModule.SystemWindowsFormsModule
+        Private visualStylesModuleWindowsFormsModule1 As DisableVisualStylesModule.Win.DisableVisualStylesModuleWindowsFormsModule
+    End Class
+End Namespace
